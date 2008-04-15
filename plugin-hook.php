9807632751bf9e6657640cc0f0d197d894089dc5
koelle <koelle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-15 19:11:15 +0000

    tagging version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/leaguemanager/tags/1.2@40795 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin-hook.php
@@ -0,0 +1,67 @@
+<?php
+/*
+Plugin Name: LeagueManager
+Plugin URI: http://wordpress.org/extend/plugins/leaguemanager/
+Description: Manage and present sports league results.
+Version: 1.2
+Author: Kolja Schleich
+
+
+Copyright 2007-2008  Kolja Schleich  (email : kolja.schleich@googlemail.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+define( 'LEAGUEMANAGER_VERSION', '1.2' );
+define( 'LEAGUEMANAGER_URL', get_bloginfo( 'wpurl' ).'/'.PLUGINDIR.'/leaguemanager' );
+
+require_once( 'leaguemanager.php' );
+
+$wpdb->leaguemanager = $table_prefix . 'leaguemanager_leagues';
+$wpdb->leaguemanager_teams = $table_prefix . 'leaguemanager_teams';
+$wpdb->leaguemanager_leaguemeta = $table_prefix . 'leaguemanager_leaguemeta';
+$wpdb->leaguemanager_teammeta = $table_prefix . 'leaguemanager_teammeta';
+$wpdb->leaguemanager_competitions = $table_prefix . 'leaguemanager_competitions';
+
+
+$leaguemanager = new WP_LeagueManager();
+
+// Actions
+add_action( 'admin_head', array(&$leaguemanager, 'add_header_code') );
+add_action( 'activate_' . basename(__FILE__, ".php") .'/' . basename(__FILE__), array(&$leaguemanager, 'init') );
+add_action( 'admin_menu', array(&$leaguemanager, 'add_admin_menu') );
+add_action( 'plugins_loaded', array(&$leaguemanager, 'init_widget') );
+	
+// Filters
+add_filter( 'the_content', array(&$leaguemanager, 'print_standings_table') );
+add_filter( 'the_content', array(&$leaguemanager, 'print_competitions_table') );
+	
+// Load textdomain for translation
+load_plugin_textdomain( 'leaguemanager', $path = PLUGINDIR.'/leaguemanager' );
+
+// Uninstall Plugin
+if ( isset($_GET['leaguemanager']) AND 'uninstall' == $_GET['leaguemanager'] AND ( isset($_GET['delete_plugin']) AND 1 == $_GET['delete_plugin'] ) )
+	$leaguemanager->uninstall();
+			
+/**
+ * Wrapper function to display widget statically
+ *
+ * @param array $args
+ */
+function leaguemanager_display_widget( $args = array() )
+{
+	global $leaguemanager;
+	$leaguemanager->display_widget( $args );
+}
+?>
