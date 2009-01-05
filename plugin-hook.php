a40269996bc416d7b0ae0e098eb624dd459f6c19
koelle <koelle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-05 11:17:34 +0000

    fixing new version
    git-svn-id: https://plugins.svn.wordpress.org/leaguemanager/trunk@84272 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin-hook.php
+++ plugin-hook.php
@@ -31,7 +31,7 @@ if ( !defined( 'WP_CONTENT_DIR' ) )
 if ( !defined( 'WP_PLUGIN_DIR' ) )
 	define( 'WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins' );
 	
-define( 'LEAGUEMANAGER_VERSION', '2.4' );
+define( 'LEAGUEMANAGER_VERSION', '2.4.1' );
 define( 'LEAGUEMANAGER_URL', WP_PLUGIN_URL.'/leaguemanager' );
 define( 'LEAGUEMANAGER_PATH', WP_PLUGIN_DIR.'/leaguemanager' );
 
