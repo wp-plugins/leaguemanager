c26ba0f5bf19b225d9fbabd323c121d83472a3df
koelle <koelle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-21 20:40:56 +0000

    git-svn-id: https://plugins.svn.wordpress.org/leaguemanager/trunk@96490 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -217,9 +217,9 @@ class LeagueManagerShortcodes extends LeagueManager
 			$points2 = maybe_unserialize($match->points2);
 			if ( $leaguemanager->isBallGameLeague( $league->id ) ) {
 				if ( $matches[$i]->hadPenalty )
-					$matches[$i]->score = sprintf("%d:%d", $matches[$i]->penalty['home'], $matches[$i]->penalty['away'])." ".__( 'o.P.', 'leaguemanager' );
+					$matches[$i]->score = sprintf("%d:%d", $matches[$i]->penalty['home'], $matches[$i]->penalty['away'])." "._c( 'o.P.|on penalty', 'leaguemanager' );
 				elseif ( $matches[$i]->hadOvertime )
-					$matches[$i]->score = sprintf("%d:%d", $matches[$i]->overtime['home'], $matches[$i]->overtime['away'])." ".__( 'AET', 'leaguemanager' );
+					$matches[$i]->score = sprintf("%d:%d", $matches[$i]->overtime['home'], $matches[$i]->overtime['away'])." "._c( 'AET|after extra time', 'leaguemanager' );
 				else
 					$matches[$i]->score = sprintf("%d:%d", $match->home_points, $match->away_points);
 				
