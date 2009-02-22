3812f513fcfa297cf6bce0c73f960263d54ccf6d
koelle <koelle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-22 22:08:49 +0000

    git-svn-id: https://plugins.svn.wordpress.org/leaguemanager/trunk@96688 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- leaguemanager.php
+++ leaguemanager.php
@@ -1,9 +1,10 @@
 <?php
 /*
 Plugin Name: LeagueManager
-Plugin URI: http://wordpress.org/extend/plugins/leaguemanager/
+Author URI: http://kolja.galerie-neander.de/
+Plugin URI: http://kolja.galerie-neander.de/plugins/leaguemanager/
 Description: Manage and present sports league results.
-Version: 2.9-beta
+Version: 2.8
 Author: Kolja Schleich
 
 Copyright 2008-2009  Kolja Schleich  (email : kolja.schleich@googlemail.com)
