862b72b216726ca7b1caffbc27b969e1b9a0c8ce
koelle <koelle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-11 12:17:56 +0000

    git-svn-id: https://plugins.svn.wordpress.org/leaguemanager/trunk@124734 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/upgrade.php
+++ admin/upgrade.php
@@ -360,11 +360,11 @@ function leaguemanager_upgrade() {
 	}
 
 
-	if (version_compare($installed, '3.1.6', '<')) {
+	if ( version_compare($installed, '3.1.6', '<') || version_compare($installed, '3.1.7', '<') ) {
 		$teams = $wpdb->get_results( "SELECT `logo` FROM {$wpdb->leaguemanager_teams}" );
 		foreach ( $teams AS $team ) {
 			if ( !empty($team->logo) ) {
-				$wpdb->query( $wpdb->prepare( "UPDATE {$wpdb->leaguemanager_teams} SET `logo` = '%s' WHERE `id` = '%%d'", $leaguemanager->getImageUrl() .'/'. basename($team->logo)), $team->id ) );
+				$wpdb->query( $wpdb->prepare( "UPDATE {$wpdb->leaguemanager_teams} SET `logo` = '%s' WHERE `id` = '%%d'", $leaguemanager->getImageUrl() .'/'. basename($team->logo), $team->id ) );
 			}
 		}
 	}
