435689dd6bea7e6fa605bbaeb4d3732446310d1f
koelle <koelle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-24 15:12:08 +0000

    updated german translation
    git-svn-id: https://plugins.svn.wordpress.org/leaguemanager/trunk@138864 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/settings.php
+++ admin/settings.php
@@ -143,7 +143,7 @@ else :
 				<td>
 					<p><input type="checkbox" name="settings[standings][pld]" id="standings_pld" value="1" <?php checked(1, $league->standings['pld']) ?> /><label for="standings_pld" style="margin-left: 0.5em;"><?php _e( 'Played Games', 'leaguemanager' ) ?></label></p>
 					<p><input type="checkbox" name="settings[standings][won]" id="standings_won" value="1" <?php checked(1, $league->standings['won']) ?> /><label for="standings_won" style="margin-left: 0.5em;"><?php _e( 'Won Games', 'leaguemanager' ) ?></label></p>
-					<p><input type="checkbox" name="settings[standings][tie]" id="standings_tie" value="1" <?php checked(1, $league->standings['tie']) ?> /><label for="standings_tie" style="margin-left: 0.5em;"><?php _e(' Tie Games', 'leaguemanager' ) ?></label></p>
+					<p><input type="checkbox" name="settings[standings][tie]" id="standings_tie" value="1" <?php checked(1, $league->standings['tie']) ?> /><label for="standings_tie" style="margin-left: 0.5em;"><?php _e('Tie Games', 'leaguemanager' ) ?></label></p>
 					<p><input type="checkbox" name="settings[standings][lost]" id="standings_lost" value="1" <?php checked(1, $league->standings['lost']) ?> /><label for="standings_lost" style="margin-left: 0.5em;"><?php _e( 'Lost Games', 'leaguemanager' ) ?></label></p>
 				</td>
 			</tr>
