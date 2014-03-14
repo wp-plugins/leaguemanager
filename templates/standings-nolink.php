c6c7c71ed2d5fd27e4b6ee21a7a5a71410ada9b8
lamontef <lamontef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-14 10:14:21 +0000

    Replaced deprecated _c tags with _x, modified Position number in standings to start at 1, not 0.
    git-svn-id: https://plugins.svn.wordpress.org/leaguemanager/trunk@875317 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/standings-nolink.php
+++ templates/standings-nolink.php
@@ -19,7 +19,7 @@ The following variables are usable:
 
 <table class="leaguemanager standingstable" summary="" title="<?php _e( 'Standings', 'leaguemanager' ) .' '.$league->title ?>">
 <tr>
-	<th class="num"><?php echo _c( 'Pos|Position', 'leaguemanager' ) ?></th>
+	<th class="num"><?php echo _e( 'Pos', 'leaguemanager' ) ?></th>
 	<th class="num">&#160;</th>
 	<?php if ( $league->show_logo ) : ?>
 	<th class="logo">&#160;</th>
@@ -27,25 +27,25 @@ The following variables are usable:
 	
 	<th><?php _e( 'Team', 'leaguemanager' ) ?></th>
 	<?php if ( 1 == $league->standings['pld'] ) : ?>
-	<th class="num"><?php _e( 'Pld', 'leaguemanager' ) ?></th>
+	<th class="num"><?php _x( 'Pld', 'leaguemanager' ) ?></th>
 	<?php endif; ?>
 	<?php if ( 1 == $league->standings['won'] ) : ?>
-	<th class="num"><?php echo _c( 'W|Won','leaguemanager' ) ?></th>
+	<th class="num"><?php echo _x( 'W','leaguemanager' ) ?></th>
 	<?php endif; ?>
 	<?php if ( 1 == $league->standings['tie'] ) : ?>
-	<th class="num"><?php echo _c( 'T|Tied','leaguemanager' ) ?></th>
+	<th class="num"><?php echo _x( 'T','leaguemanager' ) ?></th>
 	<?php endif; ?>
 	<?php if ( 1 == $league->standings['lost'] ) : ?>
-	<th class="num"><?php echo _c( 'L|Lost','leaguemanager' ) ?></th>
+	<th class="num"><?php echo _x( 'L','leaguemanager' ) ?></th>
 	<?php endif; ?>
 	<?php do_action( 'leaguemanager_standings_header_'.$league->sport ) ?>
-	<th class="num"><?php _e( 'Pts', 'leaguemanager' ) ?></th>
+	<th class="num"><?php _x( 'Pts', 'leaguemanager' ) ?></th>
 </tr>
 <?php if ( $teams ) : ?>
 <?php foreach( $teams AS $team ) : ?>
 
 <tr class='<?php echo $team->class ?>'>
-	<td class='rank'><?php echo $team->rank ?></td>
+	<td class='rank'><?php echo ($team->rank + 1) ?></td>
 	<td class="num"><?php echo $team->status ?></td>
 	<?php if ( $league->show_logo ) : ?>
 	<td class="logo">
